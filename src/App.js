c4907a1948a4e080a9ce02849e0be6d20dd5a4d2
amartya <amartya.sk@gmail.com>
2020-04-12 21:37:56 +0700

    basic monaco plugin

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Editor from './Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Monaco!</h1>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Editor.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: 'print "Hello Python!"',
+    }
+  }
+  editorDidMount(editor, monaco) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  onChange(newValue, e) {
+    console.log('onChange', newValue, e);
+  }
+  render() {
+    const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true
+    };
+    return (
+      <MonacoEditor
+        width="800"
+        height="600"
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+      />
+    );
+  }
+}
+
+export default Editor;
